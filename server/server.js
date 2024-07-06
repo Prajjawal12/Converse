9fc527693d8b78cea5f8d3b69fd2f32a7bf3a7c3
Prajjawal <prajjawaldy.zhcet@gmail.com>
2024-07-06 11:55:47 +0530

    code ready for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import authRoutes from '../server/routes/auth_routes.js';
@@ -7,6 +8,8 @@ import messageRoutes from '../server/routes/message_routes.js';
 import userRoutes from '../server/routes/user_routes.js';
 import { app, server } from './socket/socket.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
+
+const __dirname = path.resolve();
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
@@ -16,6 +19,11 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
